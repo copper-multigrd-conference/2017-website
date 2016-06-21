c71829459c54554abf3bb46ea07f1de4ac704f40
lukeolson <luke.olson@gmail.com>
2016-06-21 12:07:33 -0500

    add conference specific data

--- make.py
+++ make.py
@@ -9,8 +9,11 @@ html = env.get_template('_index.html').render(data)
 with open('index.html', 'w') as f:
     f.write(html)
 
-with open("./data/student-paper-winners.yml", "r") as inf:
+with open("./data-common/student-paper-winners.yml", "r") as inf:
     student_paper_winners = yaml.load(inf)
 
-with open("./data/previous-conferences.yml", "r") as inf:
+with open("./data-common/previous-conferences.yml", "r") as inf:
     previous_conferences = yaml.load(inf)
+
+with open("./data-conf/committee.yml", "r") as inf:
+    committee = yaml.load(inf)
