7a88f652ac330e88255f8b8d9c47c1b48b64c443
lukeolson <luke.olson@gmail.com>
2016-06-21 21:56:40 -0500

    clean up.  update script

--- make.py
+++ make.py
@@ -1,14 +1,24 @@
-from jinja2 import Environment, FileSystemLoader
+import os
+import time
 import yaml
+import shutil
+from jinja2 import Environment, FileSystemLoader
 
+# make the live web directory if needed
+# move old to a timestamp just in case
+liveweb = './live'
+
+if os.path.exists(liveweb):
+    timestamp = time.strftime('%c').replace(' ', '-')
+    shutil.move(liveweb, liveweb+'-'+timestamp)
+
+os.makedirs(liveweb)
+
+# parse, render each template here
 env = Environment(loader=FileSystemLoader('./'))
+files = ['_index.html']
 
-data = {'description': 'This is the index page.'}
-html = env.get_template('_index.html').render(data)
-
-with open('index.html', 'w') as f:
-    f.write(html)
-
+# Load Data
 with open("./data-common/student-paper-winners.yml", "r") as inf:
     student_paper_winners = yaml.load(inf)
 
@@ -17,3 +27,13 @@ with open("./data-common/previous-conferences.yml", "r") as inf:
 
 with open("./data-conf/committee.yml", "r") as inf:
     committee = yaml.load(inf)
+
+for f in files:
+    html = env.get_template(f).render()
+    with open(os.path.join('./live/', f[1:]), 'w') as fout:
+        fout.write(html)
+
+# copy these directories as-is to the webdir
+livedirs = ['font-awesome', 'bootstrap', 'css', 'images']
+for d in livedirs:
+    shutil.copytree(d, os.path.join(liveweb, d))
